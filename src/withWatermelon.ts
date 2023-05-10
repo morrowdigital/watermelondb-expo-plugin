82e206a627026f2243c127cc3fa699f60e490467
孤独な 宇宙飛行士 <50376577+TheLonelyAstronaut@users.noreply.github.com>
2023-05-10 06:58:35 +0300

    Update withWatermelon.ts

--- src/withWatermelon.ts
+++ src/withWatermelon.ts
@@ -52,10 +52,7 @@ function setAppDelegate(config: ExportedConfigWithProps) {
         `#import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import <React/RCTViewManager.h>
-#import <React/RCTBridgeModule.h>
-
-// Silence warning
-#import "../../node_modules/@nozbe/watermelondb/native/ios/WatermelonDB/SupportingFiles/Bridging.h"\n
+#import <React/RCTBridgeModule.h>"\n
             ` + contents;
 
       await fs.writeFile(filePath, updated);
