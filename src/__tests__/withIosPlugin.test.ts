61b33633e24b5435980811be461d264164b950d0
Costas Ioannou <ioannou.dev@gmail.com>
2024-04-28 11:20:57 +0300

    add test for ios Simulator exclude

--- src/__tests__/withIosPlugin.test.ts
+++ src/__tests__/withIosPlugin.test.ts
@@ -5,25 +5,35 @@ import {setExcludedArchitectures} from "../withExcludedSimulatorArchitectures";
 
 describe('iOS plugin', () => {
     let PodfileContents: string;
-    let XCodeProject: { pbxXCBuildConfigurationSection: () => any};
-    let XCodeProjectConfigurations: string;
+
+    // TODO: Can this be typed better?
+    let XCodeProject: {
+        pbxXCBuildConfigurationSection: () => any
+    };
+    // TODO: Can this be typed better?
+    let XCodeProjectConfigurations: object;
+    let XCodeProjectConfigurationsFixture: string;
 
     beforeAll(async () => {
         PodfileContents = await fs.readFile(
             path.resolve(__dirname, './fixtures/Podfile'),
             'utf-8');
+
+        XCodeProjectConfigurationsFixture = await fs.readFile(
+            // Contents of this json file were obtained from a vanilla expo project
+            path.resolve(__dirname, './fixtures/xcodeproject.config.json'),
+            'utf-8');
+
     })
 
     beforeEach(async () => {
+        XCodeProjectConfigurations = JSON.parse(XCodeProjectConfigurationsFixture);
         XCodeProject = {
             pbxXCBuildConfigurationSection(){
-                return JSON.parse(XCodeProjectConfigurations);
+                return XCodeProjectConfigurations;
             }
         }
 
-        XCodeProjectConfigurations = await fs.readFile(
-            path.resolve(__dirname, './fixtures/xcodeproject.config.json'),
-            'utf-8');
 
     })
 
@@ -40,8 +50,14 @@ describe('iOS plugin', () => {
 
     it('should be able to update the XCode project with excluded architectures', async () => {
         setExcludedArchitectures(XCodeProject);
-
         expect(XCodeProject.pbxXCBuildConfigurationSection()).toMatchSnapshot();
     })
+
+    it('should be able to update the XCode project even if run twice', async () => {
+        setExcludedArchitectures(XCodeProject);
+        setExcludedArchitectures(XCodeProject);
+        expect(XCodeProject.pbxXCBuildConfigurationSection()).toMatchSnapshot();
+    })
+
 
 })
