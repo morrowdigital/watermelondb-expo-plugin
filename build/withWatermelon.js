5a8bd87747c912a25fa56ba85117a8e877510dda
Costas Ioannou <ioannou.dev@gmail.com>
2025-11-09 09:18:01 +0200

    cleaned up

--- build/withWatermelon.js
+++ build/withWatermelon.js
@@ -6,28 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.withSDK50 = withSDK50;
 const config_plugins_1 = require("@expo/config-plugins");
 const fs_1 = __importDefault(require("fs"));
-const path_1 = __importDefault(require("path"));
-const resolve_from_1 = __importDefault(require("resolve-from"));
-const insertLinesHelper_1 = require("./insertLinesHelper");
 const fs = fs_1.default.promises;
-/**
- * Version 50+
- *  */
-function setAndroidMainApplication(config) {
-    return (0, config_plugins_1.withDangerousMod)(config, [
-        "android",
-        async (config) => {
-            // @ts-ignore
-            const extension = config.sdkVersion >= '50.0.0' ? 'kt' : 'java';
-            const root = config.modRequest.platformProjectRoot;
-            const filePath = `${root}/app/src/main/java/${config?.android?.package?.replace(/\./g, "/")}/MainApplication.${extension}`;
-            const contents = await fs.readFile(filePath, "utf-8");
-            let updated = (0, insertLinesHelper_1.insertLinesHelper)("import com.nozbe.watermelondb.WatermelonDBPackage;", "import java.util.List;", contents);
-            await fs.writeFile(filePath, updated);
-            return config;
-        },
-    ]);
-}
 function settingGradle(gradleConfig) {
     return (0, config_plugins_1.withSettingsGradle)(gradleConfig, (mod) => {
         if (!mod.modResults.contents.includes(':watermelondb-jsi')) {
@@ -53,23 +32,6 @@ function buildGradle(config) {
         return mod;
     });
 }
-const cocoaPods = (config) => {
-    return (0, config_plugins_1.withDangerousMod)(config, [
-        "ios",
-        async (config) => {
-            const filePath = path_1.default.join(config.modRequest.platformProjectRoot, "Podfile");
-            const contents = await fs.readFile(filePath, "utf-8");
-            const newContents = contents.replace('post_install do |installer|', `
-          
-    # WatermelonDB dependency
-    pod 'simdjson', path: '../node_modules/@nozbe/simdjson', modular_headers: true          
-    
-    post_install do |installer|`);
-            await fs.writeFile(filePath, newContents);
-            return config;
-        },
-    ]);
-};
 function mainApplication(config) {
     return (0, config_plugins_1.withMainApplication)(config, (mod) => {
         if (!mod.modResults.contents.includes("import com.nozbe.watermelondb.jsi.WatermelonDBJSIPackage")) {
@@ -100,91 +62,36 @@ function proGuardRules(config) {
             return config;
         }]);
 }
-/**
- * Version 50+ finish
- *  */
-/**
- * Platform: Android
- *  */
-function addFlipperDb(config, databases) {
-    return (0, config_plugins_1.withDangerousMod)(config, [
-        "android",
-        async (config) => {
-            // short circuit if no databases specified
-            if (!databases?.length)
-                return config;
-            const root = config.modRequest.platformProjectRoot;
-            const filePath = `${root}/app/src/debug/java/${config?.android?.package?.replace(/\./g, "/")}/ReactNativeFlipper.java`;
-            const contents = await fs.readFile(filePath, "utf-8");
-            // Add imports
-            let updated = (0, insertLinesHelper_1.insertLinesHelper)(`import com.facebook.flipper.plugins.databases.impl.SqliteDatabaseDriver;
-import com.facebook.flipper.plugins.databases.impl.SqliteDatabaseProvider;
-import java.io.File;
-import java.util.List;
-import java.util.ArrayList;`, "import okhttp3.OkHttpClient;", contents);
-            // Replace DatabasesFlipperPlugin with custom driver
-            const addDatabases = databases
-                .map((d) => `databaseFiles.add(new File(context.getDatabasePath("${d}").getPath().replace("/databases", "")));`)
-                .join("\n          ");
-            updated = (0, insertLinesHelper_1.insertLinesHelper)(`      client.addPlugin(new DatabasesFlipperPlugin(new SqliteDatabaseDriver(context, new SqliteDatabaseProvider() {
-        @Override
-        public List<File> getDatabaseFiles() {
-          List<File> databaseFiles = new ArrayList<>();
-          for (String databaseName : context.databaseList()) {
-            databaseFiles.add(context.getDatabasePath(databaseName));
-          }
-          ${addDatabases}
-          return databaseFiles;
-        }
-      })));`, "client.addPlugin(new DatabasesFlipperPlugin(context));", updated, 0, // replace original plugin
-            1);
-            await fs.writeFile(filePath, updated);
-            return config;
-        },
-    ]);
-}
-function setWmelonBridgingHeader(config) {
-    return (0, config_plugins_1.withDangerousMod)(config, [
-        "ios",
-        async (config) => {
-            const filePath = getPlatformProjectFilePath(config, "wmelon.swift");
-            const contents = `
+// const withCocoaPods = (config: ExpoConfig): ExpoConfig => {
+//   return withDangerousMod(config, [
+//     "ios",
+//     async (config) => {
+//       const filePath = path.join(
+//         config.modRequest.platformProjectRoot,
+//         "Podfile"
+//       );
 //
-//  water.swift
-//  watermelonDB
+//       const contents = await fs.readFile(filePath, "utf-8");
 //
-//  Created by Watermelon-plugin on ${new Date().toLocaleDateString()}.
+//       const watermelonPath = isWatermelonDBInstalled(
+//         config.modRequest.projectRoot
+//       );
 //
-
-import Foundation`;
-            await fs.writeFile(filePath, contents);
-            return config;
-        },
-    ]);
-}
-const withCocoaPods = (config) => {
-    return (0, config_plugins_1.withDangerousMod)(config, [
-        "ios",
-        async (config) => {
-            const filePath = path_1.default.join(config.modRequest.platformProjectRoot, "Podfile");
-            const contents = await fs.readFile(filePath, "utf-8");
-            const watermelonPath = isWatermelonDBInstalled(config.modRequest.projectRoot);
-            if (watermelonPath) {
-                if (!contents.includes("pod 'simdjson'")) {
-                    const patchKey = "post_install";
-                    const slicedContent = contents.split(patchKey);
-                    slicedContent[0] += `\n
-  pod 'simdjson', path: File.join(File.dirname(\`node --print "require.resolve('@nozbe/simdjson/package.json')"\`)), :modular_headers => true \n\n  `;
-                    await fs.writeFile(filePath, slicedContent.join(patchKey));
-                }
-            }
-            else {
-                throw new Error("Please make sure you have watermelondb installed");
-            }
-            return config;
-        },
-    ]);
-};
+//       if (watermelonPath) {
+//         if (!contents.includes("pod 'simdjson'")) {
+//           const patchKey = "post_install";
+//           const slicedContent = contents.split(patchKey);
+//           slicedContent[0] += `\n
+//   pod 'simdjson', path: File.join(File.dirname(\`node --print "require.resolve('@nozbe/simdjson/package.json')"\`)), :modular_headers => true \n\n  `;
+//           await fs.writeFile(filePath, slicedContent.join(patchKey));
+//         }
+//       } else {
+//         throw new Error("Please make sure you have watermelondb installed");
+//       }
+//       return config;
+//     },
+//   ]) as ExpoConfig;
+// };
 /**
  * Exclude building for arm64 on simulator devices in the pbxproj project.
  * Without this, production builds targeting simulators will fail.
@@ -210,61 +117,25 @@ const withExcludedSimulatorArchitectures = (c) => {
         return config;
     });
 };
-function isWatermelonDBInstalled(projectRoot) {
-    const resolved = resolve_from_1.default.silent(projectRoot, "@nozbe/watermelondb/package.json");
-    return resolved ? path_1.default.dirname(resolved) : null;
-}
-function getPlatformProjectFilePath(config, fileName) {
-    const projectName = config.modRequest.projectName || config.name.replace(/[- ]/g, "");
-    return path_1.default.join(config.modRequest.platformProjectRoot, projectName, fileName);
-}
-const withWatermelonDBAndroidJSI = (config, options) => {
-    if (options?.disableJsi === true) {
-        return config;
-    }
-    ;
-    function buildGradle(gradleConfig) {
-        return (0, config_plugins_1.withAppBuildGradle)(gradleConfig, (mod) => {
-            if (!mod.modResults.contents.includes("pickFirst '**/libc++_shared.so'")) {
-                mod.modResults.contents = mod.modResults.contents.replace('android {', `
-          android {
-            packagingOptions {
-               pickFirst '**/libc++_shared.so' 
-            }
-          `);
-            }
-            if (!mod.modResults.contents.includes("implementation project(':watermelondb-jsi')")) {
-                mod.modResults.contents = mod.modResults.contents.replace('dependencies {', `
-          dependencies {
-            implementation project(':watermelondb-jsi')
-          `);
-            }
-            return mod;
-        });
-    }
-    function mainApplication(mainAppConfig) {
-        return (0, config_plugins_1.withMainApplication)(mainAppConfig, (mod) => {
-            if (!mod.modResults.contents.includes('import com.nozbe.watermelondb.jsi.WatermelonDBJSIPackage;')) {
-                mod.modResults.contents = mod.modResults.contents.replace('import com.nozbe.watermelondb.WatermelonDBPackage;', `
-          import com.nozbe.watermelondb.WatermelonDBPackage;
-          import com.nozbe.watermelondb.jsi.WatermelonDBJSIPackage;
-          import com.facebook.react.bridge.JSIModulePackage;
-        `);
-            }
-            if (!mod.modResults.contents.includes('return new WatermelonDBJSIPackage()')) {
-                mod.modResults.contents = mod.modResults.contents.replace('new ReactNativeHostWrapper(this, new DefaultReactNativeHost(this) {', `
-          new ReactNativeHostWrapper(this, new DefaultReactNativeHost(this) {
-            @Override
-             protected JSIModulePackage getJSIModulePackage() {
-               return new WatermelonDBJSIPackage(); 
-             }
-          `);
-            }
-            return mod;
-        });
-    }
-    return mainApplication(settingGradle(buildGradle(config)));
-};
+// function isWatermelonDBInstalled(projectRoot: string) {
+//   const resolved = resolveFrom.silent(
+//     projectRoot,
+//     "@nozbe/watermelondb/package.json"
+//   );
+//   return resolved ? path.dirname(resolved) : null;
+// }
+// function getPlatformProjectFilePath(
+//   config: ExportedConfigWithProps,
+//   fileName: string
+// ) {
+//   const projectName =
+//     config.modRequest.projectName || config.name.replace(/[- ]/g, "");
+//   return path.join(
+//     config.modRequest.platformProjectRoot,
+//     projectName,
+//     fileName
+//   );
+// }
 // @ts-ignore
 function withSDK50(options) {
     return (config) => {
@@ -277,7 +148,7 @@ function withSDK50(options) {
             currentConfig = mainApplication(currentConfig);
         }
         // iOS
-        currentConfig = withCocoaPods(currentConfig);
+        // currentConfig = withCocoaPods(currentConfig);
         if (options?.excludeSimArch === true) {
             currentConfig = withExcludedSimulatorArchitectures(currentConfig);
         }
@@ -286,16 +157,5 @@ function withSDK50(options) {
 }
 // @ts-ignore
 exports.default = (config, options) => {
-    if (config.sdkVersion >= '50.0.0') {
-        return withSDK50(options)(config);
-    }
-    ;
-    if (config.sdkVersion < '50.0.0') {
-        config = setAndroidMainApplication(config);
-        config = addFlipperDb(config, options?.databases ?? []);
-        config = withWatermelonDBAndroidJSI(setWmelonBridgingHeader(config), options);
-        config = withCocoaPods(config);
-        config = withExcludedSimulatorArchitectures(config);
-    }
-    return config;
+    return withSDK50(options)(config);
 };
